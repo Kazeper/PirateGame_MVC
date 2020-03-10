d7bd0287916958e26017e28b1374ab6b32c44b6f
Kazeper <45803395+Kazeper@users.noreply.github.com>
2020-03-10 19:18:33 +0100

    add queue test

--- PirateGame.tests/GameTests.cs
+++ PirateGame.tests/GameTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace PirateGame.tests
 {
@@ -11,7 +12,13 @@ namespace PirateGame.tests
 
 	public class GameTests
 	{
+		private readonly ITestOutputHelper _stdOut;
 		public int NumberOfPlayers = 5;
+
+		public GameTests(ITestOutputHelper stdOut)
+		{
+			_stdOut = stdOut;
+		}
 
 		[Fact]
 		public void PlayersQueueIsCorrect()
@@ -25,12 +32,34 @@ namespace PirateGame.tests
 			}
 
 			//Act
-			Game game = new Game(players);
+			Game game = new Game(players); //Game class creates playerQueue
 
 			//Assert
-			for (int i = 0; i < NumberOfPlayers - 2; i++)
+			for (int i = 0; i < NumberOfPlayers; i++)
 			{
-				Assert.Equal(game.playerQueue[i] + 1, game.playerQueue[i + 1]);
+				_stdOut.WriteLine(game.playerQueue[i].ToString());
+
+				if (game.playerQueue[i] == players.Count)
+				{
+					if (i == (game.playerQueue.Length - 1))
+					{
+						Assert.Equal(1, game.playerQueue[0]);
+					}
+					else
+					{
+						Assert.Equal(1, game.playerQueue[i + 1]);
+					}
+					continue;
+				}
+
+				if (i == (game.playerQueue.Length - 1))
+				{
+					Assert.Equal(game.playerQueue[i] + 1, game.playerQueue[0]);
+				}
+				else
+				{
+					Assert.Equal(game.playerQueue[i] + 1, game.playerQueue[i + 1]);
+				}
 			}
 		}
 	}
