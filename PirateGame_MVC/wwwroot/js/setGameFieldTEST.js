aad00461b9a5a685962d19e5ac2e674833b7d029
Kazeper <45803395+Kazeper@users.noreply.github.com>
2020-03-26 19:25:20 +0100

    update setGameField js

--- PirateGame_MVC/wwwroot/js/setGameFieldTEST.js
+++ PirateGame_MVC/wwwroot/js/setGameFieldTEST.js
@@ -3,13 +3,19 @@
 }
 function drag(e) {
 	e.dataTransfer.setData("handler", e.target.id);
+	e.dataTransfer.setData("handlerParent", e.target.parentElement.id);
 }
 
 function drop(e) {
 	e.preventDefault();
 	var data = e.dataTransfer.getData("handler");
-	var targetId = e.target.id;
+	var parentID = e.dataTransfer.getData("handlerParent");
+
+	const parent = document.getElementById(parentID);
+
+	parent.appendChild(e.target.childNodes[2]);
 	e.target.appendChild(document.getElementById(data));
 
-	(document.getElementById(data)).appendChild(target);
+	//(document.getElementById(data)).appendChild(target);
+	//parent.appendChild(document.getElementById('#' + e.target.id + ' .img-fluid'));
 }
